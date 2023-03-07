7ea5d2534c320ea5e9934367a95fc095e2b33116
evgenii klimashko <klimashko108@gmail.com>
2023-03-07 17:24:27 +0300

    Дополнил проверку заполнения формы

--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -7,47 +7,50 @@ import time
 
 
 def test_reg_form(browser_management):
+    '''
+        Заполнение формы, удаление рекламы
+    '''
     browser.open('/')
     browser.element('#firstName').should(be.blank).type('John')
     browser.element('[id="lastName"]').should(be.blank).type('Smith')
     browser.element('[id="userEmail"]').should(be.blank).type('john_smith_980yt304gy@gmail.com')
     browser.element('[for = "gender-radio-1"]').click()
     browser.element('[id="userNumber"]').should(be.blank).type('9079079079')
-    #time.sleep(3)
     browser.element('[id="dateOfBirthInput"]').send_keys()
     browser.element('[class="react-datepicker__year-select"]').click()
-    #time.sleep(3)
     browser.element('[value="1986"]').click()
     browser.element('[class="react-datepicker__month-select"]').click()
     browser.element('[value="5"]').click()
     browser.element('[aria-label="Choose Sunday, June 15th, 1986"]').click()
-    #time.sleep(3)
     browser.driver.execute_script('window.scrollBy(0, 800)')
     browser.element('[id="subjectsInput"]').should(be.blank).type('m')
-    #time.sleep(3)
     browser.element('[tabindex="-1"]').click()
-    #time.sleep(3)
-
     browser.element('[for="hobbies-checkbox-1"]').click()
     browser.element('[for="hobbies-checkbox-2"]').click()
     browser.element('[id="uploadPicture"]').send_keys(os.getcwd()+'/picture1.jpg')
-    #time.sleep(3)
     browser.element('[id="currentAddress"]').should(be.blank).type('1478 Custer Street')
-    time.sleep(2)
-    browser.element('[id="adplus-anchor"]').execute_script('element.remove()') #Удаляет рекламный баннер
-    time.sleep(2)
+    browser.element('[id="adplus-anchor"]').execute_script('element.remove()') #Удалить рекламный баннер
     browser.element('[id="state"]').click() #открыть список для выбора штата
     browser.element('[id="react-select-3-option-1"]').click() #выбрать штат
     browser.element('[id="city"]').click()
     browser.element('[id="react-select-4-option-0"]').click()
-    time.sleep(5)
     browser.element('#submit').perform(command.js.click) #Нажать кнопку Submit
-    time.sleep(300)
+    '''
+    Проверка того, что форма заполнена корректно
+    '''
+    browser.element('//tbody/tr[1]/td[2]').should(have.text('John Smith'))
+    browser.element('//tbody/tr[2]/td[2]').should(have.text('john_smith_980yt304gy@gmail.com'))
+    browser.element('//tbody/tr[3]/td[2]').should(have.text('Male'))
+    browser.element('//tbody/tr[4]/td[2]').should(have.text('9079079079'))
+    browser.element('//tbody/tr[5]/td[2]').should(have.text('15 June,1986'))
+    browser.element('//tbody/tr[6]/td[2]').should(have.text('Maths'))
+    browser.element('//tbody/tr[7]/td[2]').should(have.text('Sports, Reading'))
+    browser.element('//tbody/tr[8]/td[2]').should(have.text('picture1.jpg'))
+    browser.element('//tbody/tr[9]/td[2]').should(have.text('1478 Custer Street'))
+    browser.element('//tbody/tr[10]/td[2]').should(have.text('Uttar Pradesh Agra'))
+    browser.element('[id="closeLargeModal"]').perform(command.js.click)  # Нажать кнопку Close
+    time.sleep(5)
 
-    #Проверка того, что форма заполнена корректно
-    #browser.element('#firstName').should(have.text('John'))
-    #browser.element('[id="lastName"]').hould(have.text('Smith'))
-    time.sleep(3)
 
 
-    #browser.element('[id="closeLargeModal"]').perform(command.js.click)  # Нажать кнопку Close
+
