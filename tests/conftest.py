c931b481e3809ccdc3e075b32c33092a60289182
evgenii klimashko <klimashko108@gmail.com>
2023-03-07 03:30:06 +0300

    Добавил удаление

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,4 +5,7 @@ from selene import browser
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
     browser.config.hold_browser_open = True
+    browser.config.window_width = '1024'
+    browser.config.window_height = '768'
+    browser.config.timeout = 6.0
     browser.config.base_url = "https://demoqa.com/automation-practice-form"
--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -1,4 +1,4 @@
-from selene import browser
+from selene import browser, command
 from selene import be, have
 import os
 import time
@@ -22,7 +22,7 @@ def test_reg_form(browser_management):
     browser.element('[value="5"]').click()
     browser.element('[aria-label="Choose Sunday, June 15th, 1986"]').click()
     #time.sleep(3)
-    browser.driver.execute_script('window.scrollBy(0, 500)')
+    browser.driver.execute_script('window.scrollBy(0, 800)')
     browser.element('[id="subjectsInput"]').should(be.blank).type('m')
     #time.sleep(3)
     browser.element('[tabindex="-1"]').click()
@@ -30,20 +30,20 @@ def test_reg_form(browser_management):
 
     browser.element('[for="hobbies-checkbox-1"]').click()
     browser.element('[for="hobbies-checkbox-2"]').click()
-    #time.sleep(3)
-    #browser.element('[id="uploadPicture"]').double_click() # можно убрать
     browser.element('[id="uploadPicture"]').send_keys(os.getcwd()+'/picture1.jpg')
     #time.sleep(3)
     browser.element('[id="currentAddress"]').should(be.blank).type('1478 Custer Street')
-    #time.sleep(3)
+    time.sleep(5)
+    browser.element('[id="adplus-anchor"]').execute_script('element.remove()') #Удаляет рекламный баннер
+    time.sleep(5)
     browser.element('[id="state"]').click() #открыть список для выбора штата
     browser.element('[id="react-select-3-option-1"]').click() #выбрать штат
-    #time.sleep(2)
     browser.element('[id="city"]').click()
-    # time.sleep(3)
     browser.element('[id="react-select-4-option-0"]').click()
-    time.sleep(10)
-    #browser.element('[for="hobbies-checkbox-2"]').click()
+    time.sleep(3)
 
-#<div class=" css-1hwfws3"><div class=" css-1wa3eu0-placeholder">Select State</div><div class="css-1g6gooi"><div class="" style="display: inline-block;"><input autocapitalize="none" autocomplete="off" autocorrect="off" id="react-select-3-input" spellcheck="false" tabindex="0" type="text" aria-autocomplete="list" value="" style="box-sizing: content-box; width: 2px; background: 0px center; border: 0px; font-size: inherit; opacity: 1; outline: 0px; padding: 0px; color: inherit;"><div style="position: absolute; top: 0px; left: 0px; visibility: hidden; height: 0px; overflow: scroll; white-space: pre; font-size: 16px; font-family: -apple-system, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, &quot;Helvetica Neue&quot;, Arial, &quot;Noto Sans&quot;, sans-serif, &quot;Apple Color Emoji&quot;, &quot;Segoe UI Emoji&quot;, &quot;Segoe UI Symbol&quot;, &quot;Noto Color Emoji&quot;; font-weight: 400; font-style: normal; letter-spacing: normal; text-transform: none;"></div></div></div></div>
-# browser.driver.execute_script('window.scrollBy(0, 500)')
+    #Проверка того, что форма заполнена корректно
+    browser.element('#firstName').should(have.text('John'))
+    browser.element('[id="lastName"]').hould(have.text('Smith'))
+
+
